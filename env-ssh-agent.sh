7a40e3f948c5f41f2511b7de30797443d59811fd
Che-Liang Chiou <clchiou@gmail.com>
2017-04-03 12:48:11 -0400

    Add env-ssh-agent.sh for setting up ssh-agent

---
+++ env-ssh-agent.sh
@@ -0,0 +1,23 @@
+# Determine if ssh-agent is running properly
+export SSH_AGENT_FILE="${HOME}/.ssh/agent-stuff.${HOSTNAME}"
+
+# Try to copy the existing agent stuff into the environment
+if [[ -r "${SSH_AGENT_FILE}" ]]; then
+  source "${SSH_AGENT_FILE}" && ssh-add -l > /dev/null && return
+fi
+
+# Start ssh-agent, and put stuff into the environment
+ssh-agent | grep -v "^echo Agent pid" > "${SSH_AGENT_FILE}"
+source "${SSH_AGENT_FILE}"
+
+# Add identities
+for _identity in $(find -L "${HOME}/.ssh" -maxdepth 1 -readable); do
+  if file "${_identity}" | grep -q 'private key'; then
+    read -t 10 -p "Add identity $(basename "${_identity}") [Y/n]? " _yes
+    if [[ "${_yes,,}" = 'y' || -z "${_yes}" ]]; then
+      ssh-add "${_identity}"
+    fi
+  fi
+done
+unset -v _identity
+unset -v _yes
