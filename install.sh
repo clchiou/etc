c8c0606976def7ba0d0ffa544a89b901af570eab
Che-Liang Chiou <clchiou@gmail.com>
2017-04-03 17:32:59 -0400

    Make use of ~/.config/git

--- install.sh
+++ install.sh
@@ -7,25 +7,24 @@ abspath() {
   cd "${curdir}"
 }
 
-# Compute ${ETC} before we `cd` to ${HOME}
+# Compute ${ETC} relative to ${HOME} before we `cd` to ${HOME}
 ETC="$(abspath "$(dirname $0)")"
 ETC="${ETC#${HOME}/}"
 
 cd "${HOME}"
 
+# Create symlinks for dot files
 DOTFILES=(
   .bash_aliases
   .bash_completion
   .bash_completion.d
   .gitconfig
-  .gitexcludes
   .hgrc
   .jslintrc
   .pylintrc
   .tmux.conf
   .vimrc
 )
-
 for dotfile in "${DOTFILES[@]}"; do
   src="${ETC%%/}/dot${dotfile}"
   if [[ -e "${dotfile}" ]]; then
@@ -36,6 +35,16 @@ for dotfile in "${DOTFILES[@]}"; do
   fi
 done
 
+# Create symlink from ~/.config/git
+mkdir -p ".config"
+if [[ -e ".config/git" ]]; then
+  echo "Skip .config/git"
+else
+  echo "Link .config/git"
+  ln -s "../${ETC%%/}/git" ".config/git"
+fi
+
+# Patch ~/.bashrc
 if cmp -s /etc/skel/.bashrc .bashrc; then
   echo "Patch .bashrc"
   patch < "${ETC%%/}/dot.bashrc.patch"
@@ -43,8 +52,7 @@ else
   echo "Different from /etc/skel/.bashrc. Skip patching .bashrc"
 fi
 
-# Install Vim plugins.
-
+# Install Vim plugins
 if [[ ! -d "${HOME}/.vim/bundle/Vundle.vim" ]]; then
   echo "Install Vundle"
   mkdir -p "${HOME}/.vim/bundle"
