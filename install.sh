5e3ea9a8d5d0d82910961fa4dd7b2ceb55a5642d
Che-Liang Chiou <clchiou@gmail.com>
2024-11-09 18:04:51 +0800

    Refactor install.sh

--- install.sh
+++ install.sh
@@ -1,61 +1,61 @@
 #!/usr/bin/env bash
 
-# Compute ${ETC} relative to ${HOME} before we `cd` to ${HOME}
-ETC="$(realpath "$(dirname $0)")"
-ETC="${ETC#${HOME}/}"
+set -o errexit -o nounset -o pipefail
 
+# Compute ${ETC} relative to ${HOME} before `cd`-ing to ${HOME}.
+readonly ETC="$(realpath --relative-to="${HOME}" "$(dirname $0)")"
 cd "${HOME}"
 
-# Create symlinks for dot files
-DOTFILES=(
+# Create dotfile symlinks.
+readonly DOTFILES=(
   .bash_aliases
   .gitconfig
   .pylintrc
   .style.yapf
   .tmux.conf
   .vimrc
+
+  .cargo/config.toml
+  .config/git
 )
 for dotfile in "${DOTFILES[@]}"; do
-  src="${ETC%%/}/dot${dotfile}"
   if [[ -e "${dotfile}" ]]; then
-    echo "Skip ${dotfile}"
-  else
-    echo "Link ${src}"
-    ln -s "${src}" "${dotfile}"
+    echo "skip: ${dotfile}"
+    continue
   fi
+
+  # It is subtle, but do not use `dirname` here.
+  dst_dir="${dotfile%/*}"
+  if [[ "${dotfile}" != ${dst_dir} ]]; then
+    if [[ ! -d "${dst_dir}" ]]; then
+      echo "mkdir: ${dst_dir}"
+      mkdir --parents "${dst_dir}"
+    fi
+    rel_path="$(realpath --relative-to="${dst_dir}" .)/"
+  else
+    rel_path=
+  fi
+
+  src="${rel_path}${ETC}/dot${dotfile}"
+  echo "link: ${src}"
+  ln --symbolic "${src}" "${dotfile}"
 done
 
-# Create symlink from ~/.config/git
-mkdir -p ".config"
-if [[ -e ".config/git" ]]; then
-  echo "Skip .config/git"
-else
-  echo "Link .config/git"
-  ln -s "../${ETC%%/}/git" ".config/git"
-fi
-
-# Create symlink from ~/.cargo/config.toml
-mkdir -p ".cargo"
-if [[ -e ".cargo/config.toml" ]]; then
-  echo "Skip .cargo/config.toml"
-else
-  echo "Link .cargo/config.toml"
-  ln -s "../${ETC%%/}/dot.cargo/config.toml" ".cargo/config.toml"
-fi
+# This seems like a good practice.
+chmod 0700 .config
 
 # Patch ~/.bashrc
-if cmp -s /etc/skel/.bashrc .bashrc; then
-  echo "Patch .bashrc"
-  patch < "${ETC%%/}/dot.bashrc.patch"
+if cmp --silent /etc/skel/.bashrc .bashrc; then
+  echo "patch: .bashrc"
+  patch < "${ETC}/dot.bashrc.patch"
 else
-  echo "Different from /etc/skel/.bashrc. Skip patching .bashrc"
+  echo "skip patching: .bashrc"
 fi
 
 # Install Vim plugins
-if [[ ! -d "${HOME}/.vim/bundle/Vundle.vim" ]]; then
-  echo "Install Vundle"
-  mkdir -p "${HOME}/.vim/bundle"
-  git clone "https://github.com/VundleVim/Vundle.vim.git" \
-    "${HOME}/.vim/bundle/Vundle.vim"
+if [[ ! -d .vim/bundle/Vundle.vim ]]; then
+  echo "install vundle"
+  mkdir --parents .vim/bundle
+  git clone https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
   vim +PluginInstall +qall
 fi
